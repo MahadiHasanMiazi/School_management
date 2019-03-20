585a1f3181cfca3970cdb36344880d9ae251ace5
Mahadi Hasan <mahadih384@gmail.com>
2019-03-21 01:24:39 +0600

    dashbord

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,36 +1,15 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-        <link href="{{asset('css/homeNavBar.css')}}" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-    <nav class="mb-4 navbar navbar-expand-lg navbar-dark cyan">
-        <a class="navbar-brand font-bold" href="#">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4" aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#"><i class="fa fa-envelope"></i> Contact <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="fa fa-gear"></i> Settings</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user"></i> Profile </a>
-                    <div class="dropdown-menu dropdown-menu-right dropdown-cyan" aria-labelledby="navbarDropdownMenuLink-4">
-                        <a class="dropdown-item" href="#">My account</a>
-                        <a class="dropdown-item" href="#">Log out</a>
-                    </div>
-                </li>
-            </ul>
+<div id="wrapper">
+@extends('layout.side-bar')
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <!-- Page Heading -->
+        <div class="row" id="main" >
+            <div class="col-sm-12 col-md-12 well" id="content">
+                <h1>Welcome Admin!</h1>
+            </div>
         </div>
-    </nav>
-
-
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    </body>
-</html>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+</div>
